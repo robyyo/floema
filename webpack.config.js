9fe7bd5ecfe05dce5add3278c03a6a786318196c
Robert Finkley <me@robertfinkley.com>
2022-01-23 09:23:33 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin'); //copies webpack files from one place to another
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //extracts css from js files *not that important*
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
+const dirApp = path.join(__dirname, 'app');
+const dirShared = path.join(__dirname, 'shared');
+const dirStyles = path.join(__dirname, 'styles');
+const dirNode = 'node_modules';
+
+module.exports = {
+  entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: '',
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name(file) {
+            return '[hash].[ext]';
+          },
+        },
+      },
+    ],
+  },
+};
