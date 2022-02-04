df1e4901b267b171d7faa64e43331e222d9e5378
Robert Finkley <me@robertfinkley.com>
2022-02-04 16:37:07 -0500

    setting up prismic

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const prismic = require('@prismicio/client');
+import * as prismicH from '@prismicio/helpers';
+import { client } from './config/prismicConfig.js';
 const path = require('path');
 const app = express();
 const port = 3000;
+
+const handleLinkResolver = (doc) => {
+  // if (doc.type === 'page') return `/${doc.lang}/${doc.uid}`;
+  // if (doc.type === 'homepage') return `/${doc.lang}`;
+  return '/';
+};
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    prismicH,
+  };
+  next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
