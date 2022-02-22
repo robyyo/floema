37cf1c5111b9baaf95a1226e68acb68ee8122f38
Robert Finkley <rfinkley82@gmail.com>
2022-02-21 20:05:41 -0500

    added static public folder

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const logger = require('morgan');
-const express = require('express');
-const prismic = require('@prismicio/client');
-const prismicH = require('@prismicio/helpers');
-const fetch = require('node-fetch');
-const path = require('path');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+require("dotenv").config();
+const logger = require("morgan");
+const express = require("express");
+const prismic = require("@prismicio/client");
+const prismicH = require("@prismicio/helpers");
+const fetch = require("node-fetch");
+const path = require("path");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const app = express();
 const port = 3000;
 const repoName = process.env.PRIMISC_REPO; // Fill in your repository name.
@@ -14,8 +14,8 @@ const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
 const endpoint = prismic.getEndpoint(repoName);
 const routes = [
   {
-    type: 'about',
-    path: '/:uid',
+    type: "about",
+    path: "/:uid",
   },
 ];
 
@@ -27,16 +27,16 @@ const client = prismic.createClient(endpoint, {
 
 const handleLinkResolver = (doc) => {
   // if (doc.type === 'page') return `/${doc.lang}/${doc.uid}`;
-  if (doc.type === 'product') return `/detail/${doc.slug}`;
-  if (doc.type === 'about') return `/about`;
-  if (doc.type === 'collections') return `/collections`;
-  return '/';
+  if (doc.type === "product") return `/detail/${doc.slug}`;
+  if (doc.type === "about") return `/about`;
+  if (doc.type === "collections") return `/collections`;
+  return "/";
 };
 
 const handleRequest = async () => {
-  const meta = await client.getSingle('meta');
-  const navigation = await client.getSingle('navigation');
-  const preloader = await client.getSingle('preloader');
+  const meta = await client.getSingle("meta");
+  const navigation = await client.getSingle("navigation");
+  const preloader = await client.getSingle("preloader");
   return {
     meta,
     navigation,
@@ -44,9 +44,10 @@ const handleRequest = async () => {
   };
 };
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride());
 
 app.use((req, res, next) => {
@@ -58,53 +59,53 @@ app.use((req, res, next) => {
 
   res.locals.Numbers = (index) => {
     return index === 0
-      ? 'One'
+      ? "One"
       : index === 1
-      ? 'Two'
+      ? "Two"
       : index === 2
-      ? 'Three'
+      ? "Three"
       : index === 3
-      ? 'Four'
-      : '';
+      ? "Four"
+      : "";
   };
 
   next();
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const defaults = await handleRequest();
-  const home = await client.getSingle('home');
-  const collections = await client.getAllByType('collection', {
-    fetchLinks: 'product.image',
+  const home = await client.getSingle("home");
+  const collections = await client.getAllByType("collection", {
+    fetchLinks: "product.image",
   });
   console.log(defaults);
-  res.render('pages/home', { ...defaults, home, collections });
+  res.render("pages/home", { ...defaults, home, collections });
 });
 
-app.get('/about', async (req, res) => {
+app.get("/about", async (req, res) => {
   const defaults = await handleRequest();
-  const about = await client.getSingle('about');
-  res.render('pages/about', { ...defaults, about });
+  const about = await client.getSingle("about");
+  res.render("pages/about", { ...defaults, about });
 });
 
-app.get('/detail/:uid', async (req, res) => {
+app.get("/detail/:uid", async (req, res) => {
   const defaults = await handleRequest();
-  const product = await client.getByUID('product', req.params.uid, {
-    fetchLinks: 'collection.title',
+  const product = await client.getByUID("product", req.params.uid, {
+    fetchLinks: "collection.title",
   });
-  res.render('pages/detail', { ...defaults, product });
+  res.render("pages/detail", { ...defaults, product });
 });
 
-app.get('/collections', async (req, res) => {
+app.get("/collections", async (req, res) => {
   const defaults = await handleRequest();
-  const collections = await client.getAllByType('collection', {
-    fetchLinks: 'product.image',
+  const collections = await client.getAllByType("collection", {
+    fetchLinks: "product.image",
   });
-  const home = await client.getSingle('home');
-  res.render('pages/collections', {
+  const home = await client.getSingle("home");
+  res.render("pages/collections", {
     ...defaults,
     collections,
     home,
